11050eb513cddbc0e8ad0641169e39773a9fa3e2
Giovanna França <giovi.g@outlook.com>
2025-05-22 21:13:14 -0300

    first commit

---
+++ src/main/java/com/giovanna/atividade_CI/AtividadeCiApplication.java
@@ -0,0 +1,13 @@
+package com.giovanna.atividade_CI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtividadeCiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtividadeCiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/giovanna/atividade_CI/AtividadeCiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.giovanna.atividade_CI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtividadeCiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
